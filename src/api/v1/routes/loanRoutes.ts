fb2d793b0b21b2f1f03fcbc069d04a1bd1a4c488
sania parvej <sparvej@rrc.ca>
2026-03-18 17:53:32 -0500

    feat: add PUT /loans/:id route for updating loan.

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { getAllLoans } from "../controllers/loanController";
 import { createLoan } from "../controllers/loanController";
+import { updateLoan } from "../controllers/loanController";
 
 const router = Router();
 
 router.get("/loans", getAllLoans);
 router.post("/loans", createLoan);
+router.put("/loans/:id", updateLoan);
 
 export default router;
