30669da0c8bc94959ec3ef2b32490219624f233e
sania parvej <sparvej@rrc.ca>
2026-03-18 17:27:57 -0500

    feat: add POST /loans route for creating loan applications.

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getAllLoans } from "../controllers/loanController";
+import { createLoan } from "../controllers/loanController";
 
 const router = Router();
 
 router.get("/loans", getAllLoans);
+router.post("/loans", createLoan);
 
 export default router;
