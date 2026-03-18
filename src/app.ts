bc9b463416f8124aac0967d0b455383c233d5b30
sania parvej <sparvej@rrc.ca>
2026-03-18 17:22:34 -0500

    feat: connected routes in app.ts.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from "express";
+import loanRoutes from "./api/v1/routes/loanRoutes";
 
 const app = express();
 
 // middleware to parse JSON
 app.use(express.json());
 
-// test route
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+//loan routes 
+app.use("/api/v1", loanRoutes);
+
 
 export default app;
