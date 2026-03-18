c3843e4a1284a9fff842a481576cd450a4c4db95
sania parvej <sparvej@rrc.ca>
2026-03-18 17:56:53 -0500

    feat: add DELETE /loans/:id route for deleting loan

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { getAllLoans } from "../controllers/loanController";
 import { createLoan } from "../controllers/loanController";
 import { updateLoan } from "../controllers/loanController";
+import { deleteLoan } from "../controllers/loanController";
 
 const router = Router();
 
 router.get("/loans", getAllLoans);
 router.post("/loans", createLoan);
 router.put("/loans/:id", updateLoan);
+router.delete("/loans/:id", deleteLoan);
 
 export default router;
